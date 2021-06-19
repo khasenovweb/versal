78bd087200f34cdaa4be688dc80137319b879a91
khasenovweb <hasenovkirill93@gmail.com>
2021-06-19 17:03:44 +0500

    sdhjdgfjhg

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,7 @@
- 
+ $(document).ready(function(){
+    $('.portfolio__item__img__slider').owlCarousel({
+        items: 1,
+        nav:true,
+        navText: ["<img src=\"img/arrow-left.svg\">","<img src=\"img/arrow-right.svg\" >"],
+    });
+ });
